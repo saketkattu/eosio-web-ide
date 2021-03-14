11b05eecc852029cd0017ce07b6081bd2fe6bc02
Saket Kattuboina <saket.kattuboina@gmail.com>
2021-03-14 12:45:01 +0000

    Made template for smart contacts

---
+++ contract/donate/donate.cpp
@@ -0,0 +1,31 @@
+#include<eosio/eosio.hpp>
+using namespace eosio;
+using namespace std;
+
+// Here are the thing we need to d in this smart contract 
+// 1. 
+// 2.
+// 3.
+// 4.
+bool isDoner(name accType){
+
+}
+
+class [[eosio::contract]] donate : public contract {
+   
+  public:
+     using contract::contract;
+
+    
+
+
+
+
+
+
+  private :
+
+
+
+
+}; 
