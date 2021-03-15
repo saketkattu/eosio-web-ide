10a11db67b0c1ffe25523762cfe2a42cb9835803
Saket Kattuboina <saket.kattuboina@gmail.com>
2021-03-15 13:30:23 +0000

    Started with volunteer

--- contract/Volunteer/volunteer.cpp
+++ contract/Volunteer/volunteer.cpp
@@ -3,10 +3,17 @@ using namespace eosio;
 using namespace std;
 
 // Here are the thing we need to d in this smart contract 
-// 1. 
-// 2.
-// 3.
-// 4.
+// 1. Check if Volunteer
+//Auth
+//Action 1: term agreement
+//Action 2: Violation of terms
+//Multi-index table :
+//id( Primary )
+//Name
+//How many hours per week can you work
+//Age
+//NGO Name (Secondary index)
+
 bool isVolunter(name accType){
 
 }
@@ -16,12 +23,11 @@ class [[eosio::contract]] volunteer : public contract {
   public:
      using contract::contract;
 
-    
-
-
-
-
-
+     [[eosio::action]] void accept_terms(){
+         
+        require_auth(volunteer);
+        
+     }
 
   private :
 
