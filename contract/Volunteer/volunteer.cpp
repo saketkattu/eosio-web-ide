a3395d9df7b7b1eacca7635893d590b4b26efdb6
Saket Kattuboina <saket.kattuboina@gmail.com>
2021-03-15 18:00:52 +0000

    Finished volunteer.cpp

--- contract/Volunteer/volunteer.cpp
+++ contract/Volunteer/volunteer.cpp
@@ -1,6 +1,8 @@
 #include<eosio/eosio.hpp>
+#include <eosio/crypto.hpp>
 using namespace eosio;
 using namespace std;
+
 
 // Here are the thing we need to d in this smart contract 
 // 1. Check if Volunteer
@@ -14,22 +16,75 @@ using namespace std;
 //Age
 //NGO Name (Secondary index)
 
-bool isVolunter(name accType){
-
+checksum256 string_to_hash(const string &input)
+{
+  return sha256(input.c_str(), input.size());
 }
+
 
 class [[eosio::contract]] volunteer : public contract {
    
   public:
      using contract::contract;
 
-     [[eosio::action]] void accept_terms(){
+     [[eosio::action]] void accept_terms(name user,string name,uint64_t time ,uint16_t age){
          
-        require_auth(volunteer);
+        check(has_auth(name("NGO");
+        //Need to create a account with NGO 
+
+        volunteer_table _volunteer(get_self(),get_self.value());
+
+        //if the record doesnt exist 
+        _volunteer.emplace(get_self(),[&] auto &row{
+            row.id=string_to_hash(name);
+            row.name=name;
+            row.time=time;
+            row.age=age;
+            row.ngo=user;
+
+        });
+
+    [[eosio::action]] void revoke(name user,string name,uint64_t time ,uint16_t age,string ngo) {
+
+        volunteer_table _volunteer(get_self(),get_self.value());
+        idx=string_to_hash(name);
+        auto itr=_volunteer.begin();
+        while (itr != _volunteer.end())
+        {
+        if (itr->volunteer == idx)
+        {
+        itr = _volunteer.erase(itr);
+        return;
+      }
+      else
+      {
+      itr++;
+      }
+  }
+
+
+    }
+    
         
      }
 
   private :
+
+  struct [[eosio::table]] volunteer{
+      checksum256 id;
+      string name;
+      uint64_t time;
+      uint16_t age;
+      string ngo ;
+   
+      checksum256 primary_key() const {return id.value;}
+      uint64_t get_secondary_key() const {return ngo;}
+  };
+
+  typedef multi_index <name("volunteer"),volunteer> volunteer_table;
+
+
+
 
 
 
